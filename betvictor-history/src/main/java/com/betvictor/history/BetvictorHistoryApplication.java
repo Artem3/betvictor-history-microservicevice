2ad2f4504f1dc204db1bc0e52905568cbe7347d4
Artem Goncharenko <artem.gocharenko@external.mercell.com>
2024-04-18 15:43:48 +0200

    init commit

---
+++ betvictor-history/src/main/java/com/betvictor/history/BetvictorHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.betvictor.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetvictorHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetvictorHistoryApplication.class, args);
+	}
+
+}
---
+++ betvictor-history/src/test/java/com/betvictor/history/BetvictorHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betvictor.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetvictorHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
